24fca171c9778d1c5d206bc7bbd657dea22f8c0c
Julio <julioces@gmail.com>
2019-06-26 07:56:21 -0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+
+import Agenda from './src/screen/Agenda'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Agenda);
